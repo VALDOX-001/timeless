e038b47b3e33613ad5f25374d83882acc461646c
Valentine E. Shadi-Sesay <officalvalentine@gmail.com>
2025-06-14 11:14:14 +0100

    Adebby commit

--- services/actor_services.py
+++ services/actor_services.py
@@ -1,13 +1,12 @@
+from typing import Any
 from sqlalchemy.orm import Session
 from models import Actor
 import schemas
 
 
-
-
 def create_actor(db: Session, actor_data: schemas.ActorCreate):
     actors = Actor(
-    name= actor_data.name,
+    name=actor_data.name,
     gender=actor_data.gender,
     date_of_birth=actor_data.date_of_birth
     )
@@ -25,11 +24,11 @@ def get_actor_by_id(db: Session, actor_id: int):
 
 
 def update_actor(db:Session, actor_id: int, actor_data: schemas.ActorUpdate):
-    actor = get_actor_by_id(db, actor_id)
+    actor: Any | None = get_actor_by_id(db, actor_id)
     if not actor:
         return None
     for key, value in actor_data.model_dump().items():
-    setattr(actor, key, value)
+        setattr(actor, key, value)
     db.commit()
     db.refresh(actor)
     return actor
